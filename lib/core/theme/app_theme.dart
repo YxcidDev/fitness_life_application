6674228feb4f03094c2d50c05a4078ee22f92c8a
YxcidDev <ederyarevalo@gmail.com>
2026-04-16 15:39:52 -0500

    feat(core): configurar tema material con paleta de colores global
    Objeto ThemeData centralizado que consume la paleta de app_colors.dart. Aplica estilos globales a MaterialApp.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+ 
+final appTheme = ThemeData(
+  fontFamily: 'Roboto',
+  colorScheme: ColorScheme.fromSeed(seedColor: kOrange),
+  scaffoldBackgroundColor: kWhite,
+);
