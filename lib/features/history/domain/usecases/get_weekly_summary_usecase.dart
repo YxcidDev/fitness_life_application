5e926fea65a45b526f92f87455e015ab91a118ba
YxcidDev <ederyarevalo@gmail.com>
2026-05-20 22:34:04 -0500

    feat(branding): actualizar iconos de la aplicación
    Actualiza los iconos launcher de Android y los App Icons de iOS
    para reflejar la identidad visual final de Fitness Life. Agrega los
    nuevos recursos drawable y reemplaza los assets anteriores del
    AppIcon.appiconset utilizados por la aplicación.

--- lib/features/history/domain/usecases/get_weekly_summary_usecase.dart
+++
@@ -1,9 +0,0 @@
-import '../repositories/i_history_repository.dart';
- 
-class GetWeeklySummaryUseCase {
-  final IHistoryRepository _repository;
- 
-  const GetWeeklySummaryUseCase(this._repository);
- 
-  Future<Map<String, double>> execute() => _repository.getWeeklySummary();
-}
